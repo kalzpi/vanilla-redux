3e6ff3bdfda4989e58b7b624fbd5b24b8ca351c2
kalzpi <kalzpi@gmail.com>
2020-04-25 18:23:24 -0500

    createAction, createReducer, configureStore

--- src/store.js
+++ src/store.js
@@ -1,35 +1,20 @@
 import { createStore } from "redux";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// Important!! When you using createReducer, you can MUTATE the state.
+// addToDo는 state를 mutate하고있다. return하는게 아니다. deleteToDo는 state를 filter한뒤 바로 return한다. 중괄호 차이 주목할것.
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      const newState = state.filter((toDo) => toDo.id !== action.id);
-      return newState;
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
