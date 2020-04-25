0637bb5a2b7c2ebff27986c8beab2d48f86d0c18
kalzpi <kalzpi@gmail.com>
2020-04-25 16:54:24 -0500

    mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home(props) {
-  console.log(props);
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -18,13 +19,18 @@ function Home(props) {
         <input type="text" value={text} onChange={onChange}></input>
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-function getCurrentState(state) {
+function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(getCurrentState)(Home);
+// mapDispatchToProps는 addToDo를 ownProps에 포함시킨다. addToDo는 함수 인자로서, store의 actionCreators 중 addToDO action을 부르는 function이다.
+function mapDispatchToProps(dispatch) {
+  return { addToDo: (text) => dispatch(actionCreators.addToDo(text)) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
